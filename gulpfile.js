037dd5176ccdb5998a7e852e979f427056dcde3c
Corey McDaniel <corey.r.mcdaniel@live.com>
2015-10-05 16:24:15 -0700

    Added gulpfile functions for watch and build functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,57 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+    pattern: ['gulp-*', 'gulp.*', 'add-stream'],
     replaceString: /\bgulp[\-.]/
 });
 
-var dest = 'build/';
+var paths = {
+    scripts: ['app/app.module.js', 'app/shared/**/*.js', 'app/components/**/*.js'],
+    ngscriptPath: 'assets/js/',
+    ngscriptName: 'combined-scripts.js',
+    index: './index.html',
+    partials: 'app/**/*.html',
+    build: 'build/'
+};
 
-// concatenate and minify all js files
-gulp.task('js', function () {
-    var jsFiles = ['assets/js/*'];
-
-    gulp.src(plugins.mainBowerFiles().concat(jsFiles))
-        .pipe(plugins.filter('*.js'))
-        .pipe(plugins.concat('main.js'))
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest(dest + 'js'));
+// concatenate all AngularJS files
+gulp.task('bundle-scripts', function () {
+    gulp.src(paths.scripts)
+        .pipe(plugins.addStream.obj(partialsToTemplates()))
+        .pipe(plugins.concat(paths.ngscriptName))        
+        .pipe(gulp.dest(paths.ngscriptPath));
 });
 
-gulp.task('css', function () {
-    var cssFiles = ['assets/css/*'];
+function partialsToTemplates() {
+    return gulp.src(paths.partials)
+    .pipe(plugins.htmlmin({collapseWhitespace: true, removeComments: true}))
+    .pipe(plugins.angularTemplatecache());
+}
 
-    gulp.src(plugins.mainBowerFiles().concat(cssFiles))
-        .pipe(plugins.filter('*.css'))
-        .pipe(plugins.order([
-        'boostrap.css',
-        '*'
-    ]))
-        .pipe(plugins.concat('main.css'))
-        .pipe(plugins.autoprefixer('last 2 versions'))
-        .pipe(plugins.minifyCss())
-        .pipe(gulp.dest(dest + 'css'));
+gulp.task('usemin', function () {
+    gulp.src(paths.index)
+        .pipe(plugins.usemin({
+            css: ['concat', plugins.purifycss(paths.scripts.concat(paths.index), {
+                info: true,
+                rejected: true
+            }), plugins.autoprefixer('last 2 versions'), plugins.minifyCss({
+                keepSpecialComments: 0
+            })],
+            vendor: ['concat', plugins.stripDebug(), plugins.uglify()],
+            js: ['concat', plugins.stripDebug(), plugins.uglify()]
+        }))
+        .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('clean', function () {
+    gulp.src([paths.build, paths.ngscriptPath + paths.ngscriptName], {
+            read: false
+        })
+        .pipe(plugins.clean());
+});
+
+gulp.task('build', ['bundle-scripts', 'usemin']);
+
+gulp.task('default', ['bundle-scripts'], function() {
+    gulp.watch(paths.partials, ['bundle-scripts']);
+    gulp.watch(paths.scripts, ['bundle-scripts']);
 });
