7c54aff4692834d6023e794d507e4a01488ae2da
Corey McDaniel <corey.r.mcdaniel@live.com>
2015-10-13 14:21:19 -0700

    Serving build files for gh-pages

--- app/shared/navbar.controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('NavbarCtrl', ['$scope', '$location', function($scope, $location) {
-    $scope.isCollapsed = true;
-    $scope.isActive = function(route) {
-        return route === $location.path();
-    };
-}]);
